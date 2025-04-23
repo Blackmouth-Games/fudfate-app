300fc7b4333f50f5c0d5025e5c31fcf6a0905301
CPS_Produ <cps.blackmouth@gmai.com>
2025-04-23 10:39:23 +0200

    fix: vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +14,13 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        // Ensure the _redirects file is copied to the dist folder
+        fs.copyFileSync('_redirects', 'dist/_redirects');
+      }
+    }
   ].filter(Boolean),
   resolve: {
     alias: {
